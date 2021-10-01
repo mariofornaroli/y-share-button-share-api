4c05d06a0c555b022eb37e615ff3e4955b5ccd04
Mario Fornaroli <mario.fornaroli@leitha.eu>
2021-10-01 15:35:03 +0200

    First implementation

---
+++ script.js
@@ -0,0 +1,19 @@
+const shareBtn = document.getElementById('shareBtn')
+
+shareBtn.addEventListener('click', event => {
+
+  // Check for Web Share api support
+  if (navigator.share) {
+    // Browser supports native share api
+    navigator.share({
+      text: 'Please read this great article: ',
+      url: 'https://www.google.com/'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    // Fallback
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
